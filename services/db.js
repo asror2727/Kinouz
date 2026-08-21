1144b9327bbb8f9fa2f705049060a7b13f61d9b9
asror2727 <asrorake001@gmail.com>
2026-08-21 23:33:32 +0500

    Delete services directory

--- services/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-async function connect(uri) {
-  if (!uri) throw new Error("MONGODB_URI topilmadi");
-  await mongoose.connect(uri);
-  console.log("MongoDB ulandi");
-}
-
-module.exports = { connect };
