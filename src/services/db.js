ceebc5cbbe232dfe7a76328df8303a7b35262dbf
asror2727 <asrorake001@gmail.com>
2026-08-22 00:46:29 +0500

    Refactor db.js to manage JSON data file
    Refactored database connection to include file operations for reading and writing data. Added functionality to create a JSON file if it does not exist.

--- src/services/db.js
+++ src/services/db.js
@@ -1,9 +1,14 @@
-const mongoose = require("mongoose");
+const fs = require('fs');
+const path = require('path');
 
-async function connect(uri) {
-  if (!uri) throw new Error("MONGODB_URI topilmadi");
-  await mongoose.connect(uri);
-  console.log("MongoDB ulandi");
+const dbPath = path.join(__dirname, '../../data.json');
+
+// Fayl bo'lmasa yaratib oladi
+if (!fs.existsSync(dbPath)) {
+    fs.writeFileSync(dbPath, JSON.stringify({ users: [], movies: [] }, null, 2));
 }
 
-module.exports = { connect };
+const readData = () => JSON.parse(fs.readFileSync(dbPath, 'utf8'));
+const writeData = (data) => fs.writeFileSync(dbPath, JSON.stringify(data, null, 2));
+
+module.exports = { readData, writeData };
